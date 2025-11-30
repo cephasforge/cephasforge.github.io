3043593b5a16de604d6c2138d8d1999cce5dce3d
pulsarforge <cephasbenadi@gmail.com>
2025-11-30 17:24:21 -0600

    multi converter - mass converter added

--- projects/web/multiconverter/script.js
+++ projects/web/multiconverter/script.js
@@ -1,3 +1,7 @@
+// =====================================================================================
+// VARIABLES AND CONSTANTS
+// =====================================================================================
+
 const conversionType = document.getElementById("conversion-type");
 const conversionBoxes = Array.from(document.getElementsByClassName("conversion-box"));
 
@@ -7,123 +11,223 @@ const lengthOutput = document.getElementById("length-output");
 const lenghtUnitInputOption = document.getElementById("lenght-unit-input");
 const lenghtUnitOutputOption = document.getElementById("lenght-unit-output");
 
+const massInput = document.getElementById("mass-input");
+const massOutput = document.getElementById("mass-output");
+
+const massUnitInputOption = document.getElementById("mass-unit-input");
+const massUnitOutputOption = document.getElementById("mass-unit-output");
+
+
 // =====================================================================================
 // CONVERTER INPUT LISTENER
 // =====================================================================================
 
-lenghtUnitInputOption.addEventListener('change', () => {
-    conversionLenghtCore();
-})
+    conversionType.addEventListener('change', () => {
 
-lenghtUnitOutputOption.addEventListener('change', () => {
-    conversionLenghtCore();
-})
+        hideConversionBoxes();
 
-lengthInput.addEventListener('input', () => {
-    conversionLenghtCore();
-})
+        switch (conversionType.value) {
+            case 'type-length':
+                document.getElementById("length-conversion").style.display = 'block';
+                break;
+            case 'type-mass':
+                document.getElementById("mass-conversion").style.display = 'block';
+                break;
+            case 'type-time':
+                document.getElementById("time-conversion").style.display = 'block';
+                break;
+            default:
+                break;
+        }
+    });
 
-conversionType.addEventListener('change', () => {
+    // =====================================================================================
+    // LENGHT
+    // =====================================================================================
 
-    hideConversionBoxes();
+    lenghtUnitInputOption.addEventListener('change', () => {
+        conversionLenghtCore();
+    })
 
-    switch (conversionType.value) {
-        case 'type-length':
-            document.getElementById("length-conversion").style.display = 'block';
-            break;
-        case 'type-mass':
-            document.getElementById("mass-conversion").style.display = 'block';
-            break;
-        case 'type-time':
-            document.getElementById("time-conversion").style.display = 'block';
-            break;
-        default:
-            break;
-    }
-});
+    lenghtUnitOutputOption.addEventListener('change', () => {
+        conversionLenghtCore();
+    })
+
+    lengthInput.addEventListener('input', () => {
+        conversionLenghtCore();
+    })
+
+    // =====================================================================================
+    // MASS 
+    // =====================================================================================
+
+    massUnitInputOption.addEventListener('change', () => {
+        conversionMassCore();
+    })
+
+    massUnitOutputOption.addEventListener('change', () => {
+        conversionMassCore();
+    })
+
+    massInput.addEventListener('input', () => {
+        conversionMassCore();
+    })
 
 // =====================================================================================
-// LENGHT CONVERSION
+// CONVERSION FUNCTIONS
 // =====================================================================================
 
-function meterToOthers(input, unit){
+    // =====================================================================================
+    // LENGTH
+    // =====================================================================================
 
-    let result = 0.0;
-    let parseInput = parseFloat(input);
+    function meterToOthers(input, unit){
 
-    switch (unit) {
-        case "km":
-            result = (parseInput / 1000.0);
-            return result;
-        case "m":
-            result = parseInput;
-            return result;
-        case "cm":
-            result = (parseInput * 100.0);
-            return result;
-        case "mm":
-            result = (parseInput * 1000.0);
-            return result;
+        let result = 0.0;
+        let parseInput = parseFloat(input);
 
-        case "mi":
-            result = (parseInput / 1609.344);
-            return result;
-        case "yd":
-            result = (parseInput * 1.09361329834);
-            return result;
-        case "ft":
-            result = (parseInput * 3.28083989501);
-            return result;
-        case "in":
-            result = (parseInput * 39.3700787402);
-            return result; 
-        default:
-            return result;
+        switch (unit) {
+            case "km":
+                result = (parseInput / 1000.0);
+                return result;
+            case "m":
+                result = parseInput;
+                return result;
+            case "cm":
+                result = (parseInput * 100.0);
+                return result;
+            case "mm":
+                result = (parseInput * 1000.0);
+                return result;
+
+            case "mi":
+                result = (parseInput / 1609.344);
+                return result;
+            case "yd":
+                result = (parseInput * 1.09361329834);
+                return result;
+            case "ft":
+                result = (parseInput * 3.28083989501);
+                return result;
+            case "in":
+                result = (parseInput * 39.3700787402);
+                return result; 
+            default:
+                return result;
+        }
     }
-}
 
-function othersToMeter(input, unit){
+    function othersToMeter(input, unit){
 
-    let result = 0.0;
-    let parseInput = parseFloat(input);
+        let result = 0.0;
+        let parseInput = parseFloat(input);
 
-    switch (unit) {
-        case "km":
-            result = (parseInput * 1000.0);
-            return result;
-        case "m":
-            result = parseInput;
-            return result;
-        case "cm":
-            result = (parseInput / 100.0);
-            return result;
-        case "mm":
-            result = (parseInput / 1000.0);
-            return result; 
+        switch (unit) {
+            case "km":
+                result = (parseInput * 1000.0);
+                return result;
+            case "m":
+                result = parseInput;
+                return result;
+            case "cm":
+                result = (parseInput / 100.0);
+                return result;
+            case "mm":
+                result = (parseInput / 1000.0);
+                return result; 
 
-        case "mi":
-            result = (parseInput * 1609.344);
-            return result;
-        case "yd":
-            result = (parseInput * 0.9144);
-            return result;
-        case "ft":
-            result = (parseInput * 0.3048);
-            return result;
-        case "in":
-            result = (parseInput * 0.0254);
-            return result; 
-        default:
-            return result;
+            case "mi":
+                result = (parseInput * 1609.344);
+                return result;
+            case "yd":
+                result = (parseInput * 0.9144);
+                return result;
+            case "ft":
+                result = (parseInput * 0.3048);
+                return result;
+            case "in":
+                result = (parseInput * 0.0254);
+                return result; 
+            default:
+                return result;
+        }
     }
-}
 
+    // =====================================================================================
+    // MASS
+    // =====================================================================================
+
+    function gramToOthers(input, unit){
+
+        let result = 0.0;
+        let parseInput = parseFloat(input);
+
+        switch (unit) {
+            case "t":
+                result = (parseInput / 1e6);
+                return result;
+            case "kg":
+                result = (parseInput / 1000.0);
+                return result;
+            case "g":
+                result = parseInput;
+                return result;
+            case "mg":
+                result = (parseInput * 100.0);
+                return result;
+
+            case "lb":
+                result = (parseInput / 453.59237);
+                return result;
+            case "oz":
+                result = (parseInput / 28.349523125);
+                return result;
+            case "ton":
+                result = (parseInput / 907184.74);
+                return result;
+            default:
+                return result;
+        }
+    }
+
+    function othersToGram(input, unit){
+
+        let result = 0.0;
+        let parseInput = parseFloat(input);
+
+        switch (unit) {
+            case "t":
+                result = (parseInput * 1e6);
+                return result;
+            case "kg":
+                result = (parseInput * 1000.0);
+                return result;
+            case "g":
+                result = parseInput;
+                return result;
+            case "mg":
+                result = (parseInput / 100.0);
+                return result;
+
+            case "lb":
+                result = (parseInput * 453.59237);
+                return result;
+            case "oz":
+                result = (parseInput * 28.349523125);
+                return result;
+            case "ton":
+                result = (parseInput * 907184.74);
+                return result;
+            default:
+                return result;
+        }
+    }
 // =====================================================================================
 // CONVERTER FUNCTIONS
 // =====================================================================================
 
 function conversionLenghtCore(){
-
+    
     let inputUnit = document.getElementById("lenght-unit-input").value;
     let outputUnit = document.getElementById("lenght-unit-output").value;
 
@@ -140,9 +244,32 @@ function conversionLenghtCore(){
     }
 
     if(Number.isNaN(outputValue)){
-        lengthOutput.value = 0;
+        lengthOutput.value = 0+" "+outputUnit;
     }else{
-        lengthOutput.value = Number(outputValue.toFixed(3));
+        lengthOutput.value = Number(outputValue.toFixed(3))+" "+outputUnit;
+    }
+}
+
+function conversionMassCore(){
+    let inputUnit = document.getElementById("mass-unit-input").value;
+    let outputUnit = document.getElementById("mass-unit-output").value;
+
+    let inputValue = massInput.value;
+    let outputValue = massOutput.value;
+    
+    if(inputUnit == "g"){
+        outputValue = gramToOthers(inputValue, outputUnit);
+    }else if(outputUnit == "g"){
+        outputValue = othersToGram(inputValue, inputUnit);
+    }else{
+        inputValue = othersToGram(inputValue, inputUnit);
+        outputValue = gramToOthers(inputValue, outputUnit);
+    }
+
+    if(Number.isNaN(outputValue)){
+        massOutput.value = 0+" "+outputUnit;
+    }else{
+        massOutput.value = Number(outputValue.toFixed(3))+" "+outputUnit;
     }
 }
 
